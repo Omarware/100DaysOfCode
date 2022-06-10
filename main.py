4c60c87952bc8e04287a0fbf08bd2c4e34ad6f45
Omar Salcedo Enciso <omar.salen@hotmail.com>
2022-06-10 00:58:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+new_height = float(height) 
+new_weight = float(weight)
+
+bmi = new_weight / (new_height * new_height)
+bmi = format(bmi,".2f")
+print("Your BMI is " + bmi)
+
+
+
+
+
+
+
+
+
